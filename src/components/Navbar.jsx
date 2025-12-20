b9d72eb1a1f226bae4bad3dec70519ebda06c79c
Kolade OluwaDamilola <kolade443@gmail.com>
2025-12-20 13:27:11 +0100

    update project

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ const Navbar = function () {
            
 
  
-            <nav className="glass-nav rounded-xl bg-nav bg-gradient-to-br from-[#d3cef6] to-[#e5e0f8] md:left-6 md:right-6 md:top-6 md:rounded-4xl text-white flex items-center justify-between px-4 md:px-8 py-3 md:py-4 fixed md:w-auto z-20 w-full">
+            <nav className="glass-nav mt-2 rounded-xl bg-nav bg-gradient-to-br from-[#d3cef6] to-[#e5e0f8] md:left-6 md:right-6 md:top-6 md:rounded-4xl text-white flex items-center justify-between px-4 md:px-8 py-3 md:py-4 fixed md:w-auto z-20 w-full">
   {/* Left: Logo */}
   <div className="flex items-center z-10">
     <img src="Navlogo.svg" alt="Logo" />
